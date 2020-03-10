0fb76f0b4dec2243db306ab3e265c354eba2b661
Stephen Wann <c16409712@mydit.ie>
2020-03-10 15:10:32 +0000

    Water Tower

---
+++ Tower Offense 2.0/Assets/Scripts/WaterTower.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTower : MonoBehaviour
+{
+    public Transform target;
+
+    public GameObject playerBase;
+
+    public float waterTowerRange;
+    public float waterTowerAttackSpeed;
+
+    string[] enemyTags = { "EnemyWater", "EnemyWind", "EnemyEarth" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        foreach(string tag in enemyTags)
+        {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag(tag);
+            float shortestDistance = Mathf.Infinity;
+            GameObject nearestEnemy = null;
+            
+            foreach (GameObject enemy in enemies)
+            {
+                float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+                if(distanceToEnemy < shortestDistance)
+                {
+                    shortestDistance = distanceToEnemy;
+                    nearestEnemy = enemy;
+                }
+            }
+
+            if(nearestEnemy != null && shortestDistance <= waterTowerRange)
+            {
+                target = nearestEnemy.transform;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null)
+        {
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = lookRotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, waterTowerRange);
+    }
+}
