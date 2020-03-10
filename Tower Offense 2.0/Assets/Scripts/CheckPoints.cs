beb55c11a4e2dd5f759a420d9fa19e66bf6a0ac1
Finnchris13 <finnchris13@gmail.com>
2020-03-10 08:09:12 +0000

    Created script for territory conversion and control with checkpoints

---
+++ Tower Offense 2.0/Assets/Scripts/CheckPoints.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoints : MonoBehaviour
+{
+
+    public int cpLives;
+
+    public GameObject CPholder;
+    public Transform allyWatchtower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cpLives = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(cpLives <= 0)
+        {
+            destroyFunction();
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.gameObject.tag == "Ally")
+        {
+            Debug.Log("Ow");
+            cpLives -= 1;
+        }
+
+    }
+
+    void destroyFunction()
+    {
+
+        CPholder.GetComponent<TerritoryConversion>().calculateArray();
+        Instantiate(allyWatchtower, transform.position, transform.rotation);
+        Destroy(gameObject);
+
+    }
+
+}
---
+++ Tower Offense 2.0/Assets/Scripts/TerritoryConversion.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerritoryConversion : MonoBehaviour
+{
+
+    public GameObject enemyTerritory;
+    public GameObject allyTerritory;
+
+    public GameObject[] CParray;
+
+    public bool territoryConverted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        territoryConverted = false;
+        calculateArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CParray.Length <= 1 && territoryConverted == false)
+        {
+            convertTerritory();
+        }
+    }
+
+    void convertTerritory()
+    {
+
+        enemyTerritory.SetActive(false);
+        allyTerritory.SetActive(true);
+
+        territoryConverted = true;
+    }
+
+    public void calculateArray()
+    {
+        CParray = new GameObject[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            CParray[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+}
