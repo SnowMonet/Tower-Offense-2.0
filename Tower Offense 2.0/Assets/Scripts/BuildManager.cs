99e4a5151af1a02b8d2c47c0f406d1016823bf10
Finnchris13 <finnchris13@gmail.com>
2020-03-11 12:05:15 +0000

    Added UI stuff, camera view change option.

--- Tower Offense 2.0/Assets/Scripts/BuildManager.cs
+++ Tower Offense 2.0/Assets/Scripts/BuildManager.cs
@@ -1,10 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildManager : MonoBehaviour
 {
     public static BuildManager instance;
+    public GameObject[] towerLogos = new GameObject[3];
+    public GameObject[] cameraViews = new GameObject[5];
+    public GameObject towerPanel;
+    public GameObject cameraPanel;
+    public GameObject selectedTowerPanel;
+
+    public Text selectedTowerText;
+
+    public int towerPanelActiveIndex;
+    public int cameraPanelActiveIndex;
+    public int selectedTowerPanelActiveIndex;
+    public int cameraViewIndex;
 
     void Awake()
     {
@@ -24,26 +37,28 @@ public class BuildManager : MonoBehaviour
     void Start()
     {
         towerToBuild = waterTowerPrefab;
+        cameraViewIndex = 0;
+        towerPanelActiveIndex = 0;
+        cameraPanelActiveIndex = 0;
+        selectedTowerPanelActiveIndex = 0;
+        selectedTowerText.text = "Water Tower Selected";
     }
 
     void Update()
     {
         if(Input.GetKeyDown("z"))
         {
-            Debug.Log("Water Tower Selected");
-            towerToBuild = waterTowerPrefab;
+            waterTower();
         }
 
         if (Input.GetKeyDown("x"))
         {
-            Debug.Log("Wind Tower Selected");
-            towerToBuild = windTowerPrefab;
+            windTower();
         }
 
         if (Input.GetKeyDown("c"))
         {
-            Debug.Log("Earth Tower Selected");
-            towerToBuild = earthTowerPrefab;
+            earthTower();
         }
     }
 
@@ -53,4 +68,152 @@ public class BuildManager : MonoBehaviour
     {
         return towerToBuild;
     }
+
+    public void waterTower()
+    {
+        Debug.Log("Water Tower Selected");
+        selectedTowerText.text = "Water Tower Selected";
+        towerLogos[0].SetActive(true);
+        towerLogos[1].SetActive(false);
+        towerLogos[2].SetActive(false);
+        towerToBuild = waterTowerPrefab;
+    }
+
+    public void windTower()
+    {
+        Debug.Log("Wind Tower Selected");
+        selectedTowerText.text = "Wind Tower Selected";
+        towerLogos[0].SetActive(false);
+        towerLogos[1].SetActive(true);
+        towerLogos[2].SetActive(false);
+        towerToBuild = windTowerPrefab;
+    }
+
+    public void earthTower()
+    {
+        Debug.Log("Earth Tower Selected");
+        selectedTowerText.text = "Earth Tower Selected";
+        towerLogos[0].SetActive(false);
+        towerLogos[1].SetActive(false);
+        towerLogos[2].SetActive(true);
+        towerToBuild = earthTowerPrefab;
+    }
+
+    public void closeTowerPanel()
+    {
+        towerPanelActiveIndex += 1;
+
+        if(towerPanelActiveIndex >= 2)
+        {
+            towerPanelActiveIndex = 0;
+        }
+
+        if(towerPanelActiveIndex == 0)
+        {
+            towerPanel.SetActive(true);
+        }
+
+        if (towerPanelActiveIndex == 1)
+        {
+            towerPanel.SetActive(false);
+        }
+    }
+
+    public void closeCameraPanel()
+    {
+        cameraPanelActiveIndex += 1;
+
+        if (cameraPanelActiveIndex >= 2)
+        {
+            cameraPanelActiveIndex = 0;
+        }
+
+        if (cameraPanelActiveIndex == 0)
+        {
+            cameraPanel.SetActive(true);
+        }
+
+        if (cameraPanelActiveIndex == 1)
+        {
+            cameraPanel.SetActive(false);
+        }
+    }
+
+    public void closeSelectedTowerPanel()
+    {
+        selectedTowerPanelActiveIndex += 1;
+
+        if (selectedTowerPanelActiveIndex >= 2)
+        {
+            selectedTowerPanelActiveIndex = 0;
+        }
+
+        if (selectedTowerPanelActiveIndex == 0)
+        {
+            selectedTowerPanel.SetActive(true);
+        }
+
+        if (selectedTowerPanelActiveIndex == 1)
+        {
+            selectedTowerPanel.SetActive(false);
+        }
+    }
+
+    public void changeCamera()
+    {
+
+        cameraViewIndex += 1;
+
+        if (cameraViewIndex >= 5)
+        {
+            cameraViewIndex = 0;
+        }
+
+        if (cameraViewIndex == 0)
+        {
+            cameraViews[0].SetActive(true);
+            cameraViews[1].SetActive(false);
+            cameraViews[2].SetActive(false);
+            cameraViews[3].SetActive(false);
+            cameraViews[4].SetActive(false);
+        }
+
+        if (cameraViewIndex == 1)
+        {
+            cameraViews[0].SetActive(false);
+            cameraViews[1].SetActive(true);
+            cameraViews[2].SetActive(false);
+            cameraViews[3].SetActive(false);
+            cameraViews[4].SetActive(false);
+        }
+
+        if (cameraViewIndex == 2)
+        {
+            cameraViews[0].SetActive(false);
+            cameraViews[1].SetActive(false);
+            cameraViews[2].SetActive(true);
+            cameraViews[3].SetActive(false);
+            cameraViews[4].SetActive(false);
+        }
+
+        if (cameraViewIndex == 3)
+        {
+            cameraViews[0].SetActive(false);
+            cameraViews[1].SetActive(false);
+            cameraViews[2].SetActive(false);
+            cameraViews[3].SetActive(true);
+            cameraViews[4].SetActive(false);
+        }
+
+        if (cameraViewIndex == 4)
+        {
+            cameraViews[0].SetActive(false);
+            cameraViews[1].SetActive(false);
+            cameraViews[2].SetActive(false);
+            cameraViews[3].SetActive(false);
+            cameraViews[4].SetActive(true);
+        }
+
+    }
+
 }
